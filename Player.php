7cdaef1ec00b34b71f9f2e99460804e00eb377f4
Andy Gorbould <gorbould@mail.com>
2022-06-27 12:28:48 +0200

    updated but not working yet

--- Player.php
+++ Player.php
@@ -11,32 +11,38 @@ class Player {
     public function __construct(Blackjack $deck)
         {
             $this->deck = $deck
-            $this->cardOne = $cardOne;
-            $this->cardTwo = $cardTwo;
-            $this->cardOne = $deck->drawCard();
-            $this->cardTwo = $deck->drawCard();        
+            // $cardOne = Deck::drawCard();
+            // $cardTwo = Deck::drawCard();
+            
+            public function drawCard(): Deck; {
+                return $cards = &$this->card;
+            }
         }
     
 
     // Getters
     public function hit()
     {
-        
+        Deck::drawCard();
     }
     
     public function surrender()
     {
-        
+        return $lost = True;   
     }
     
     public function getScore()
     {
-        
+        foreach ($cards as $card) {
+            getValue();
+            // combine values, if>21 return $lost = True, if<=21 return $lost = False (win) 
+
+        }
     }
     
     public function hasLost()
     {
-        
+        return $lost = True;   
     }
 
 
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Create a blackjack game in PHP using OOP.
 Properties must be private, so getters & setters are necessary.
+
+### PROBLEMS:
+In the constructor of the Player class; draw 2 cards for the player. You have to use an existing method for this from the Deck class. ----> needs each to be pushed to "private $cards" on player.php ?
